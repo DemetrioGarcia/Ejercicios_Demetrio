1653faee53449eb67d1a9f4ea9259ced6f151cba
A713\1dam <dememons@gmail.com>
2024-10-01 18:39:46 +0200

    Practica 4 Terminada

--- Ejercicios_Demetrio/src/main/java/org/example/HolaMundo.java
+++ Ejercicios_Demetrio/src/main/java/org/example/HolaMundo.java
@@ -7,7 +7,7 @@ public class HolaMundo {
     //El nombre de la clase definida por el programa HolaMundo debe ser igual que el nombre del archivo (HolaMundo.java).
         // Tu programa comienza con una llamada a main().
         // Imprime "Hola Mundo" en la ventana de la terminal.
-        public static void ejecutar (String args[])
+        public static void ejecutar ()
         // public: Para que la JVM (Máquina Virtual Java) pueda ejecutar el metodo desde cualquier lugar.
         // static: El metodo principal puede ser llamado sin crear el objeto (lo veremos más adelante)
         // void: El metodo main no devuelve nada, por lo tanto, no hay que definirlo con un tipo de datos concreto.
--- Ejercicios_Demetrio/src/main/java/org/example/Practica4.java
+++ Ejercicios_Demetrio/src/main/java/org/example/Practica4.java
@@ -5,12 +5,13 @@ import java.util.Scanner;
 
 public class Practica4 {
     public void Multiplicacion() {
-        int n1=0;
+        int n1=1000;
         int n2=0;
+        int aux=0;
         int multiplicacion;
 
         Scanner entradaEscaner = new Scanner(System.in);
-        while(n1<=0 || n2<=0) {
+        while(n1>999 || n1<-999 || n2>999 || n2<-999) {
             try {
 
                 System.out.println("Introduce un multiplicando(3 cifras):");
@@ -18,23 +19,29 @@ public class Practica4 {
 
                 System.out.println("Introduce un multiplicador(3 cifras):");
                 n2 = entradaEscaner.nextInt();
+
             } catch (NumberFormatException | InputMismatchException ex) {
                 System.out.println("Introduce un numero valido");
                 entradaEscaner.next();
             }
         }
+        int total = n1 * n2;
+        if (n1<0){
+            n1=n1*-1;
+        }
+        if (n2<0){
+            n2=n2*-1;
+        }
             String cifra1 = Integer.toString(n1);
-            String cifra = Integer.toString(n2);
-            if (cifra1.length()==3) {
-                if (cifra.length() == 3) {
-                    int total = n1 * n2;
+            String cifra2 = Integer.toString(n2);
+            if (cifra1.length()==3 && cifra2.length()==3) {
                     System.out.println("El producto de la multiplicación es: " + total);
                     System.out.println("El proceso es:");
                     System.out.println("  " + n1);
                     System.out.println("X " + n2);
                     System.out.println("________");
                     for (int i = 2; i >= 0; i--) {
-                        cifra1 = cifra.substring(i, i + 1);
+                        cifra1 = cifra2.substring(i, i + 1);
                         int cifram = Integer.parseInt(cifra1);
                             multiplicacion = cifram * n1;
                         if (i > 1) {
@@ -47,10 +54,9 @@ public class Practica4 {
                     }
                     System.out.println("________");
                     System.out.println("" + total);
-                } else {
+            }
+            else {
                     System.out.print("Los productos han de tener 3 cifras");
-                }
-
             }
     }
 }
