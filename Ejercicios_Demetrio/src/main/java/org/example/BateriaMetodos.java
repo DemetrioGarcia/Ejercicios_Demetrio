d11fa47b0dd0f3ef350d663e845e330786d06122
A713\1dam <dememons@gmail.com>
2024-12-05 19:46:58 +0100

    Bateria Metodos completa

--- Ejercicios_Demetrio/src/main/java/org/example/BateriaMetodos.java
+++ Ejercicios_Demetrio/src/main/java/org/example/BateriaMetodos.java
@@ -1,8 +1,12 @@
 package org.example;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class BateriaMetodos {
+
+    static Scanner entrada;
+
     public static void  Ejercicio1() {
 
         Scanner entrada = new Scanner(System.in);
@@ -89,62 +93,119 @@ public class BateriaMetodos {
 
     public static void Ejercicio4(){
 
-        for (){
-        float[] semana = entradatos();
-        String mayventas = masVentas(semana);
-         }
     }
 
-    public static float[] entradatos(){
-
-        float[] semana = new float[6];
-        Scanner entrada = new Scanner(System.in);
-
-        for (int i = 0; i < 6; i++) {
-            semana[i] = entrada.nextFloat();
+        public static boolean casoDePrueba() {
+            float dato = entrada.nextFloat();
+            if (dato==-1) {
+                return false;
+            }
+            else {
+                float[] semana = rellenarDatos(dato);
+                String diamay = masVentas(semana);
+                String diamenos = menosVentas(semana);
+                float media = media(semana);
+                String recaudacion = recaudacion(semana[5],media);
+                System.out.println(diamay+" "+diamenos+" "+recaudacion);
+                return true;
+            }
         }
 
-        return semana;
-    }
+        public static void main(String[] args) {
+            entrada = new java.util.Scanner(System.in);
+            while (casoDePrueba()) {
+
+            }
+        }
+
+        public static float[] rellenarDatos(float dato){
+            float[] datos = new float[6];
+
+            datos[0] = dato;
+            for (int i = 1; i < datos.length; i++) {
+                datos[i] = entrada.nextFloat();
+            }
+            return datos;
+        }
 
     public static String masVentas(float[] semana){
 
         String dia;
         int x=0;
-        int may=0;
+        float may=0;
 
-        for (int i = 1; i < semana.length; i++) {
-            if (semana[x]>semana[i]){
-                int may=x;
-            }
-            else{
-                x=i;
+        float[] semana2 = semana.clone();
+        Arrays.sort(semana2);
+
+        may = semana2[5];
+
+        for (int i = 0; i < semana.length; i++) {
+            if (may==semana[i]){
+                x = i;
             }
         }
 
-        switch (may){
-            case 1:
-
-                break;
-            case 2:
-
-                break;
-            case 3:
-
-                break;
-            case 4:
-
-                break;
-            case 5:
-
-                break;
-            case 6:
-
-                break;
-            default:
-                
-        }
+        dia = switch (x) {
+            case 0 -> "Martes";
+            case 1 -> "Miercoles";
+            case 2 -> "Jueves";
+            case 3 -> "Viernes";
+            case 4 -> "Sábado";
+            case 5 -> "Domingo";
+            default -> "ERROR";
+        };
         return dia;
     }
 
+    public static String menosVentas(float[] semana){
+
+        String dia;
+        int x=0;
+        float may=0;
+
+        float[] semana2 = semana.clone();
+        Arrays.sort(semana2);
+
+        may = semana2[0];
+
+        for (int i = 0; i < semana.length; i++) {
+            if (may==semana[i]){
+                x = i;
+            }
+        }
+
+        dia = switch (x) {
+            case 0 -> "Martes";
+            case 1 -> "Miercoles";
+            case 2 -> "Jueves";
+            case 3 -> "Viernes";
+            case 4 -> "Sábado";
+            case 5 -> "Domingo";
+            default -> "ERROR";
+        };
+        return dia;
+    }
+
+    public static float media (float[] semana){
+
+        float media = 0;
+        for (int i = 0; i < semana.length; i++) {
+             media = media + semana[i];
+        }
+        media = media/ semana.length;
+        return media;
+    }
+
+    public static String recaudacion (float domingo, float media){
+
+        String recaudacion;
+
+        if (media>domingo){
+            recaudacion = "NO";
+        }
+        else {
+            recaudacion="Sí";
+        }
+        return recaudacion;
+    }
 }
---
+++ Ejercicios_Demetrio/src/main/java/org/example/ProgramaMe_HB.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class ProgramaMe_HB {
+
+    public static void EntradaDatos(){
+
+    }
+}
