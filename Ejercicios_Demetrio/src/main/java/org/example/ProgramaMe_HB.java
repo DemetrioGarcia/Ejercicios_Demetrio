1ccb072867a4e2e92af152f3ba05c3243768e53f
A713\1dam <dememons@gmail.com>
2024-12-13 18:50:50 +0100

    Ejercicio ProgramaMe con test de errores (algunos)

--- Ejercicios_Demetrio/src/main/java/org/example/ProgramaMe_HB.java
+++ Ejercicios_Demetrio/src/main/java/org/example/ProgramaMe_HB.java
@@ -5,6 +5,10 @@ import java.util.InputMismatchException;
 import java.util.Objects;
 import java.util.Scanner;
 
+/**
+ * @author Demetrio García Ferre
+ * @version 1.0.0
+ */
 public class ProgramaMe_HB {
 
     static Scanner entrada;
@@ -15,34 +19,63 @@ public class ProgramaMe_HB {
 
         }
     }
-
+/**
+   caseDePrueba es el método principal dónde se ejecutará nuestro programa, contiene un if(aux) para cortar la ejecución del programa si el valor no cumple las condiciones.
+   @return true || false
+*/
     public static boolean casoDePrueba() {
-        boolean aux = true;
-        int tiempomax = 0;
-        while (aux) {
-            try {
-                tiempomax = Integer.parseInt(entrada.nextLine());
-                aux = false;
-            } catch (NumberFormatException e) {
-                System.out.println("ERROR");
-                entrada.nextLine();
-                aux = true;
-            }
-        }
 
-        if (tiempomax <= 0 || tiempomax > 100001) {
-            return false;
-        } else {
-            int[] felicitaciones = cadenaDatos();
-            System.out.println(Arrays.toString(felicitaciones));
-            int cant = CantFelic(felicitaciones, tiempomax);
-            System.out.println(cant);
-            return true;
-        }
+            int tiempomax = Tiempo();
+            boolean aux = ValidarTiempo(tiempomax);
+            if (aux) {
+                int[] felicitaciones = cadenaDatos();
+                System.out.println(Arrays.toString(felicitaciones));
+                int cant = CantFelic(felicitaciones, tiempomax);
+                System.out.println(cant);
+                return true;
+            }
+            else {
+                return false;
+            }
     }
 
+    /**
+        En el método Tiempo pedimos al usuario un entero si la entrada de los datos no coincide con la esperada (entero) igualamos la variable tiempomax a 0 (que como veremos más adelnante no es válido)
+        si la entrada si és válida retornaremos el valor.
+     @return tiempomax
+     */
+    public static int Tiempo () {
+        int tiempomax = 0;
+        try {
+            tiempomax = Integer.parseInt(entrada.nextLine());
+        } catch (NumberFormatException e) {
+            tiempomax = 0;
+        }
+        return tiempomax;
+    }
+    /**
+        En el método ValidarTiempo comprobamos que el número introducido cumple las condiciones:
+                  >1 && <100001, si esto no se cumple retornamos false de lo contrario true.
+        @param tiempo - pedimos un entero (tiempo) el cual recibimos del metodo anterior, este parametro nos indica la cantidad de tiempo maxima que vamos a esperar entre mensajes.
+        @return aux
+    */
+    public static boolean ValidarTiempo(int tiempo) {
+        boolean aux;
+        aux = tiempo > 0 && tiempo <= 100000;
+        return aux;
+    }
+
+    /**
+        El metodo cadenaDatos nos devolverá un array de enteros, para llegar a esta empezamos por pedir un string con las felicitaciones y lo almacenamos en un array con el metodo split " "
+        luego vamos conviertiendo los valores en enteros (siempre que no sean superiores a 10^9) como nuestra cadena acaba en 0 (Porque si no entrará al bucle y acabará ahi el programa)
+         cuando el valor del array sea 0 el cual lo ordenará y saldrá del bucle for e ira a otro dónde pasaremos los valores ordenados al segundo array (hay que tener cuidado que al ordenar el primer valor será 0, el cual deberá ir el último)
+         para no cometer ese error vamos añadiendo a tiempo2 el valor de tiempo1+1 y cuando llegamos a la ultima posicion ponemos el 0 y devolvemos tiempo2.
+
+     @return tiempo2
+    */
+
+
     public static int[] cadenaDatos() {
-//        System.out.print("Introduce una cadena que acabe en 0:");
         String felicitaciones;
         int[] tiempo2 = null;
         int[] tiempo = null;
@@ -53,6 +86,9 @@ public class ProgramaMe_HB {
             tiempo = new int[tfel.length];
             tiempo2 = new int[tiempo.length];
 
+            if (!Objects.equals(tfel[tfel.length - 1], "0")){
+                System.exit(0);
+            }
             for (int i = 0; i < tfel.length; i++) {
 
                 if (tiempo[i] > 1000000000) {
@@ -68,8 +104,7 @@ public class ProgramaMe_HB {
 
             }
         } catch (NumberFormatException e) {
-            entrada.nextLine();
-            System.out.println("ERROR");
+            System.exit(0);
         }
         for (int i = 0; i < tiempo2.length; i++) {
 
@@ -82,6 +117,13 @@ public class ProgramaMe_HB {
         return tiempo2;
     }
 
+    /**
+        Por último el metodo CantFelic nos devolvera cuantas veces tenemos que felicitar para realizar este calculo realizamos un bucle dónde vamos pasando por los números del array si el tiempomax es mayor que las respuestas pasará a la siguiente posición del array hasta que sea menor, añadimos una respuesta
+        y seguiremos recorriendo hasta que se terminen los valores del array.
+     @param datos - necesitaremos el array de datos de los tiempos que hemos recibido los mensajes (tiempo2).
+     @param tiempo - el intevalo max (tiempo max) que va ser quien nos indica cada cuanto vamos a "escrbir un mensaje".
+     @return respuestas
+     */
     public static int CantFelic(int [] datos, int tiempo){
 
         int respuestas = 0;
---
+++ Ejercicios_Demetrio/src/test/java/org/example/ProgramaMe_HBTest.java
@@ -0,0 +1,59 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.sql.Array;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProgramaMe_HBTest {
+
+    @Test
+    void main() {
+    }
+
+    @Test
+    void casoDePrueba() {
+    }
+
+    @Test
+    void cadenaDatos() {
+    }
+
+    @Test
+    void cantFelic() {
+        int[] Array = new int[]{1, 2, 3, 4, 5, 0};
+        assertAll(
+                () -> assertEquals(3 , ProgramaMe_HB.CantFelic(Array,2)),
+                () -> assertEquals(1 , ProgramaMe_HB.CantFelic(Array,5)),
+                () -> assertEquals(5 , ProgramaMe_HB.CantFelic(Array,1))
+        );
+    }
+
+    @Test
+    void tiempo() {
+
+            //simulamos que el usuario ingresa caracteres
+            String entradaSimulada = "aaa"; // 'a' es un carácter no válido
+            System.setIn(new ByteArrayInputStream(entradaSimulada.getBytes()));
+
+            //llamamos al metodo que procesa la entrada
+            int resultado = ProgramaMe_HB.Tiempo();
+
+            //verificamos que el valor que devuelve es el esperado (0)
+            assertEquals(0, resultado);
+    }
+
+    @Test
+    void validarTiempo() {
+        assertAll(
+                () -> assertFalse(ProgramaMe_HB.ValidarTiempo(0)),
+                () -> assertFalse(ProgramaMe_HB.ValidarTiempo(100001)),
+                () -> assertTrue(ProgramaMe_HB.ValidarTiempo(10))
+        );
+    }
+
+}
